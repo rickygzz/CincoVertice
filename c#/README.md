e80a8af996102d8d8bf7d87613e23d15865a6067
Ricardo Gonzalez Garza <rickygzz@hotmail.com>
2022-05-24 18:56:42 -0500

    Added C# skeleton.

---
+++ c#/README.md
@@ -0,0 +1,3 @@
+# Visual Studio and .NET Core
+
+This project was created with Visual Studio Version 2022 (17.1.2) and .NET 6.0
---
+++ c#/VerticeLib/Strings/Strings.cs
@@ -0,0 +1,40 @@
+﻿namespace VerticeLib.Strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Strings
+    {
+        /// <summary>
+        /// After. Gets the string after the delimiter.
+        /// <para>Example: After("A = B", "=", true) will return "B".</para>
+        /// </summary>
+        /// <param name="text">The string to work with.</param>
+        /// <param name="delimiter">The string to look up within text, delimiting the text before and after.</param>
+        /// <param name="trim">If true, it removes all leading and trailing white-spaces from the current string.</param>
+        /// <returns>If sucessful, gets the string after the delimiter. Otherwise, returns string.Empty if there is no characters after delimiter or no delimiter is found.</returns>
+        public static string After(string text, string delimiter, bool trim = false)
+        {
+            int textLength = text.Length;
+            int delimiterLength = delimiter.Length;
+
+            for (int i = 0; i < (textLength - delimiterLength); i++)
+            {
+                if (text.Substring(i, delimiterLength) == delimiter)
+                {
+                    if (trim)
+                    {
+                        return text.Substring(i + delimiterLength).Trim();
+                    }
+
+                    return text.Substring(i + delimiterLength);
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
