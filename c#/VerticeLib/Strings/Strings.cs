44246a66c9e7cfd6d41753f41eab061898480042
Ricardo Gonzalez Garza <rickygzz@hotmail.com>
2022-05-24 20:08:30 -0500

    Added StyleCopsAnalyzers.

--- c#/VerticeLib/Strings/Strings.cs
+++ c#/VerticeLib/Strings/Strings.cs
@@ -1,4 +1,22 @@
-﻿namespace VerticeLib.Strings
+﻿// <copyright file="Strings.cs" company="Ricardo Gonzalez-Garza and 5Vertice Team">
+// This file is part of the VerticeLib distribution (https://github.com/rickygzz/VerticeLib/).
+// Copyright (c) 2018 - 2022 Ricardo Gonzalez-Garza and 5Vertice Team. All Rights Reserved.
+// Contact ricardo@5vertice.com for additional information.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see (http://www.gnu.org/licenses/).
+// </copyright>
+
+namespace VerticeLib.Strings
 {
     using System;
     using System.Collections.Generic;
@@ -6,6 +24,9 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Strings class.
+    /// </summary>
     public class Strings
     {
         /// <summary>
